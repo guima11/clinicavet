ec3528fdb5bf58209fd92d76ed445bd569845e50
guima11 <105176137+guima11@users.noreply.github.com>
2024-11-23 23:15:30 -0300

    Fix: ajuste create todo editável

--- src/controllers/CreateConsultaController.ts
+++ src/controllers/CreateConsultaController.ts
@@ -3,10 +3,10 @@ import {CreateConsultaService} from '../services/CreateConsultaService'
 
 class CreateConsultaController{
     async handle(request: FastifyRequest, reply: FastifyReply){
-        const{name, email} = request.body as { name: string, email: string};
+        const{name, email, pago, tipo_pagamento, valor, condicao} = request.body as { name: string, email: string, pago: boolean, tipo_pagamento: string, valor: number, condicao: string};
         const consultaService = new CreateConsultaService()
 
-        const consulta = await consultaService.execute({name, email});
+        const consulta = await consultaService.execute({name, email, pago, tipo_pagamento, valor, condicao});
         reply.send(consulta)
     }
 }
